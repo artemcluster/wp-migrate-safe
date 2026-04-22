46823a8be2e79fef5c776478780fdb5c5499df11
Artem Pronin <193480189+artemcluster@users.noreply.github.com>
2026-04-22 13:06:10 +0300

    feat(rest): register REST routes under wp-migrate-safe/v1

---
+++ src/Rest/RestRouter.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace WpMigrateSafe\Rest;
+
+/**
+ * Central registry for all REST routes exposed by the plugin.
+ *
+ * Called on `rest_api_init`.
+ */
+final class RestRouter
+{
+    public static function registerRoutes(): void
+    {
+        $ns = WPMS_REST_NAMESPACE;
+
+        $upload = new UploadController();
+
+        register_rest_route($ns, '/upload/init', [
+            'methods'             => 'POST',
+            'callback'            => [$upload, 'init'],
+            'permission_callback' => [self::class, 'checkPermission'],
+            'args'                => [
+                'filename' => ['type' => 'string', 'required' => true],
+                'total_size' => ['type' => 'integer', 'required' => true],
+                'sha256' => ['type' => 'string', 'required' => true],
+            ],
+        ]);
+
+        register_rest_route($ns, '/upload/chunk', [
+            'methods'             => 'POST',
+            'callback'            => [$upload, 'chunk'],
+            'permission_callback' => [self::class, 'checkPermission'],
+            'args'                => [
+                'upload_id' => ['type' => 'string', 'required' => true],
+                'chunk_index' => ['type' => 'integer', 'required' => true],
+            ],
+        ]);
+
+        register_rest_route($ns, '/upload/complete', [
+            'methods'             => 'POST',
+            'callback'            => [$upload, 'complete'],
+            'permission_callback' => [self::class, 'checkPermission'],
+            'args'                => [
+                'upload_id' => ['type' => 'string', 'required' => true],
+            ],
+        ]);
+
+        register_rest_route($ns, '/upload/abort', [
+            'methods'             => 'POST',
+            'callback'            => [$upload, 'abort'],
+            'permission_callback' => [self::class, 'checkPermission'],
+            'args'                => [
+                'upload_id' => ['type' => 'string', 'required' => true],
+            ],
+        ]);
+
+        $backups = new BackupsController();
+
+        register_rest_route($ns, '/backups', [
+            'methods'             => 'GET',
+            'callback'            => [$backups, 'index'],
+            'permission_callback' => [self::class, 'checkPermission'],
+        ]);
+
+        register_rest_route($ns, '/backups/(?P<filename>[^/]+)', [
+            'methods'             => 'DELETE',
+            'callback'            => [$backups, 'delete'],
+            'permission_callback' => [self::class, 'checkPermission'],
+        ]);
+    }
+
+    public static function checkPermission(): bool
+    {
+        return current_user_can(WPMS_CAPABILITY);
+    }
+}
